9f515b3908aa432a52f1c267fa6fffbea9ad6d58
Lykrast <aymericpm@gmail.com>
2025-04-14 18:06:37 +0200

    Tooltip for gunsmithing table

--- src/main/java/lykrast/bypowderandsteel/block/GunsmithingTableBlock.java
+++ src/main/java/lykrast/bypowderandsteel/block/GunsmithingTableBlock.java
@@ -1,6 +1,15 @@
 package lykrast.bypowderandsteel.block;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -21,6 +30,12 @@ public class GunsmithingTableBlock extends HorizontalDirectionalBlock {
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> state) {
 		state.add(FACING);
+	}
+	
+	@Override
+	public void appendHoverText(ItemStack stack, @Nullable BlockGetter worldIn, List<Component> tooltip, TooltipFlag flagIn) {
+		super.appendHoverText(stack, worldIn, tooltip, flagIn);
+		tooltip.add(Component.translatable(getDescriptionId() + ".desc").withStyle(ChatFormatting.GRAY));
 	}
 
 }
