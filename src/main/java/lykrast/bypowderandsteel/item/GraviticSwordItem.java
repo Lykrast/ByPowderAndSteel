3598c4d33ac257a4b0454547f0fe3bbca86663c7
Lykrast <aymericpm@gmail.com>
2025-04-20 16:29:51 +0200

    Tooltip for gravitic saber

--- src/main/java/lykrast/bypowderandsteel/item/GraviticSwordItem.java
+++ src/main/java/lykrast/bypowderandsteel/item/GraviticSwordItem.java
@@ -1,5 +1,11 @@
 package lykrast.bypowderandsteel.item;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
@@ -7,6 +13,8 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 
 public class GraviticSwordItem extends SwordItem {
 
@@ -20,6 +28,12 @@ public class GraviticSwordItem extends SwordItem {
 		target.push(0, 0.5 * mult, 0);
 		target.addEffect(new MobEffectInstance(MobEffects.LEVITATION, 20, 0));
 		return super.hurtEnemy(stack, target, attacker);
+	}
+
+	@Override
+	public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
+		super.appendHoverText(stack, worldIn, tooltip, flagIn);
+		tooltip.add(Component.translatable("tooltip.bypowderandsteel.gravitic_bullet").withStyle(ChatFormatting.GRAY));
 	}
 
 }
