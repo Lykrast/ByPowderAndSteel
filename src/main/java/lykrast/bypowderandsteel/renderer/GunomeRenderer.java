e59f9180151f8657c552e9663672a6a6f09899a3
Lykrast <aymericpm@gmail.com>
2025-08-26 11:13:30 +0200

    yurf

--- src/main/java/lykrast/bypowderandsteel/renderer/GunomeRenderer.java
+++ src/main/java/lykrast/bypowderandsteel/renderer/GunomeRenderer.java
@@ -1,14 +1,17 @@
 package lykrast.bypowderandsteel.renderer;
 
+import java.util.Locale;
+
 import lykrast.bypowderandsteel.ByPowderAndSteel;
 import lykrast.bypowderandsteel.entity.GunomeEntity;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraft.resources.ResourceLocation;
 
 public class GunomeRenderer extends MobRenderer<GunomeEntity, GunomeModel> {
-	private static final ResourceLocation TEXTURE = ByPowderAndSteel.rl("textures/entity/gunome.png");
+	private static final ResourceLocation TEXTURE = ByPowderAndSteel.rl("textures/entity/gunome.png"), YURF = ByPowderAndSteel.rl("textures/entity/gunome_yurf.png");
 
 	public GunomeRenderer(EntityRendererProvider.Context context) {
 		super(context, new GunomeModel(context.bakeLayer(GunomeModel.MODEL)), 0.375f);
@@ -17,6 +20,8 @@ public class GunomeRenderer extends MobRenderer<GunomeEntity, GunomeModel> {
 
 	@Override
 	public ResourceLocation getTextureLocation(GunomeEntity entity) {
+		String name = ChatFormatting.stripFormatting(entity.getName().getString()).toLowerCase(Locale.ROOT);
+		if ("yurf".equals(name)) return YURF;
 		return TEXTURE;
 	}
 }
