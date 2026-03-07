10ef03e0b0fcdd90febd843bd004300168914c51
Lykrast <aymericpm@gmail.com>
2026-03-07 01:55:27 +0100

    Inquisitor real behavior

--- src/main/java/lykrast/bypowderandsteel/entity/ZombunnyInquisitorEntity.java
+++ src/main/java/lykrast/bypowderandsteel/entity/ZombunnyInquisitorEntity.java
@@ -3,12 +3,12 @@ package lykrast.bypowderandsteel.entity;
 import javax.annotation.Nullable;
 
 import lykrast.bypowderandsteel.ByPowderAndSteel;
-import lykrast.bypowderandsteel.entity.ai.GunGoal;
 import lykrast.bypowderandsteel.misc.BPASUtils;
 import lykrast.bypowderandsteel.registry.BPASEffects;
 import lykrast.bypowderandsteel.registry.BPASItems;
 import lykrast.bypowderandsteel.registry.BPASSounds;
 import lykrast.gunswithoutroses.item.BulletItem;
+import lykrast.gunswithoutroses.item.GunItem;
 import lykrast.gunswithoutroses.registry.GWRAttributes;
 import lykrast.gunswithoutroses.registry.GWRItems;
 import net.minecraft.core.BlockPos;
@@ -25,21 +25,19 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.MobType;
-import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
 import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.IronGolem;
-import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -47,8 +45,10 @@ import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 
-public class ZombunnyInquisitorEntity extends Monster implements GunMob {
-	//TODO port blade behavior from slasher, from now just a gunner
+public class ZombunnyInquisitorEntity extends AnimatedMonster implements GunMob {
+	//animations
+	public static final int ANIM_NEUTRAL = 0, ANIM_WINDUP = 1, ANIM_JUMP = 2, ANIM_LAND = 3, ANIM_AIM = 4, ANIM_SHOOTED = 5;
+	
 	public ZombunnyInquisitorEntity(EntityType<? extends ZombunnyInquisitorEntity> type, Level world) {
 		super(type, world);
 	}
@@ -56,7 +56,7 @@ public class ZombunnyInquisitorEntity extends Monster implements GunMob {
 	@Override
 	protected void registerGoals() {
 		goalSelector.addGoal(1, new FloatGoal(this));
-		goalSelector.addGoal(4, new ZombunnyGunGoal(this, 1, 4, 6, 7));
+		goalSelector.addGoal(4, new ChaseLeapShoot(this, 1, true));
 		goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1));
 		goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 8));
 		goalSelector.addGoal(6, new RandomLookAroundGoal(this));
@@ -78,7 +78,8 @@ public class ZombunnyInquisitorEntity extends Monster implements GunMob {
 	}
 
 	public static AttributeSupplier.Builder createAttributes() {
-		return BPASUtils.baseGunMobAttributes().add(Attributes.MAX_HEALTH, 100).add(Attributes.MOVEMENT_SPEED, 0.25).add(GWRAttributes.dmgTotal.get(), 0.5).add(GWRAttributes.fireDelay.get(), 3);
+		return BPASUtils.baseGunMobAttributes().add(Attributes.MAX_HEALTH, 80).add(Attributes.ARMOR, 10).add(Attributes.MOVEMENT_SPEED, 0.31).add(Attributes.KNOCKBACK_RESISTANCE, 1)
+				.add(GWRAttributes.dmgTotal.get(), 0.5);
 	}
 
 	@SuppressWarnings("deprecation")
@@ -111,6 +112,12 @@ public class ZombunnyInquisitorEntity extends Monster implements GunMob {
 	}
 
 	@Override
+	protected void setupNewAnimation() {
+		if (clientAnim == ANIM_WINDUP || clientAnim == ANIM_NEUTRAL || clientAnim == ANIM_AIM) animDur = 5;
+		else animDur = 2;
+	}
+
+	@Override
 	protected ResourceLocation getDefaultLootTable() {
 		return ByPowderAndSteel.rl("entities/zombunny_inquisitor");
 	}
@@ -133,7 +140,7 @@ public class ZombunnyInquisitorEntity extends Monster implements GunMob {
 			//mob attacks don't call sword hurtEnemy aaaaaa
 			//so cheating by putting the status effect here like cave spiders
 			if (target instanceof LivingEntity ltarget) {
-				ltarget.addEffect(new MobEffectInstance(BPASEffects.marked.get(), 5 * 20, 0), this);
+				ltarget.addEffect(new MobEffectInstance(BPASEffects.marked.get(), 5 * 20, 1), this);
 			}
 			return true;
 		}
@@ -160,70 +167,197 @@ public class ZombunnyInquisitorEntity extends Monster implements GunMob {
 		this.playSound(SoundEvents.ZOMBIE_STEP, 0.15F, 1);
 	}
 
-	private static class ZombunnyGunGoal extends GunGoal<ZombunnyInquisitorEntity> {
-		public ZombunnyGunGoal(ZombunnyInquisitorEntity mob, double speed, int minAttackInterval, double strafeRange, double attackRange) {
-			super(mob, speed, minAttackInterval, strafeRange, attackRange);
+	private static class ChaseLeapShoot extends MeleeAttackGoal {
+		private ZombunnyInquisitorEntity buny;
+		//0 approach, 1 windup, 2 leap, 3 recover, 4 feint leap, 5 shooting
+		private int phase;
+		//ticksUntilNextAttack is private so I'm making my own countdown
+		private int time, feints, extraShots;
+
+		public ChaseLeapShoot(ZombunnyInquisitorEntity buny, double speed, boolean seeThroughWalls) {
+			super(buny, speed, seeThroughWalls);
+			this.buny = buny;
+		}
+
+		@Override
+		public void start() {
+			super.start();
+			phase = 0;
+			time = 0;
 		}
 
 		@Override
 		public void stop() {
 			super.stop();
-			mob.setPose(Pose.STANDING);
+			buny.setAnimation(ANIM_NEUTRAL);
 		}
 
 		@Override
-		protected void doStrafing(LivingEntity target, double targetDistance) {
-			if (strafeRadiusSqr > 0 && targetDistance <= strafeRadiusSqr && seeTime >= 20) {
-				mob.getNavigation().stop();
-				//don't increment strafe while in the air so the jumps are more spaced
-				if (mob.onGround()) ++strafingTime;
-			}
-			else {
-				mob.getNavigation().moveTo(target, speedModifier);
-				strafingTime = -1;
-				mob.setPose(Pose.STANDING);
-			}
+		public void tick() {
+			if (time > 0) time--;
+			super.tick();
+		}
 
-			//mixing strafing replaced with the leaping
-			if (strafingTime >= 20) {
-				if (mob.getRandom().nextFloat() < 0.3) strafingClockwise = !strafingClockwise;
-				Vec3 vec3 = mob.getDeltaMovement();
-				Vec3 vec31 = new Vec3(target.getX() - mob.getX(), 0, target.getZ() - mob.getZ());
-				if (vec31.lengthSqr() > 1.0E-7D) {
-					vec31 = vec31.normalize().scale(0.6);
-					if (strafingBackwards) vec31 = vec31.yRot(Mth.PI);
-					else if (strafingClockwise) vec31 = vec31.yRot(Mth.HALF_PI * 0.75f); //I'm actually not sure the clockwise/counterclockwise aren't inverted but eeeeeh
-					else vec31 = vec31.yRot(-Mth.HALF_PI * 0.75f);
-					vec31 = vec31.add(vec3.scale(0.2));
-				}
-				mob.setDeltaMovement(vec31.x, 0.5, vec31.z);
-				mob.setPose(Pose.STANDING);
-				//TODO dedicated jump sound
-				mob.playSound(SoundEvents.RABBIT_JUMP, 1, 1);
-				strafingTime = 0;
-				//only mix up forward/backwards after leaping
-				if (mob.getRandom().nextFloat() < 0.3) strafingBackwards = !strafingBackwards;
-			}
-			else if (strafingTime == 10) {
-				//crouch before jumping
-				mob.setPose(Pose.CROUCHING);
-			}
+		@Override
+		protected void checkAndPerformAttack(LivingEntity target, double distanceSqr) {
+			//this is only called when target is alive, so we do the transition animations above
+			switch (phase) {
+				case 0:
+					//running at target
+					if (target.hasEffect(BPASEffects.marked.get())) {
+						//target is marked, try to shoot if los
+						if (buny.getSensing().hasLineOfSight(target)) {
+							phase = 5;
+							time = 20;
+							buny.setAnimation(ANIM_AIM);
+							buny.getLookControl().setLookAt(target, 50, 0);
+							extraShots = buny.random.nextInt(2) + buny.random.nextInt(2);
+						}
+					}
+					else if (distanceSqr < 20) {
+						//ready for the jump
+						phase = 1;
+						time = 10;
+						buny.setAnimation(ANIM_WINDUP);
+						buny.getLookControl().setLookAt(target, 50, 0);
+						feints = buny.random.nextInt(2) + buny.random.nextInt(2); //0-2 feints, weighted towards 1
+						//TODO windup sound
+					}
+					break;
+				case 1:
+					//bracing for jump
+					if (distanceSqr > 49) {
+						//target too far, disengage jump
+						//but longer than slashers because larger jump
+						phase = 0;
+						buny.setAnimation(ANIM_NEUTRAL);
+					}
+					else if (time <= 0) {
+						//leap!
+						time = 10;
+						//from LeapAtTargetGoal
+						Vec3 vec3 = buny.getDeltaMovement();
+						Vec3 vec31 = new Vec3(target.getX() - buny.getX(), 0, target.getZ() - buny.getZ());
+						if (feints > 0) {
+							feints--;
+							phase = 4;
+							if (vec31.lengthSqr() > 1.0E-7D) {
+								vec31 = vec31.normalize().scale(1).add(vec3.scale(0.2)).yRot((buny.random.nextBoolean() ? -1 : 1)* Mth.HALF_PI * 0.5f);
+							}
+						}
+						else {
+							phase = 2;
+							buny.setAnimation(ANIM_JUMP);
+							if (vec31.lengthSqr() > 1.0E-7D) {
+								//bigger leap than slashers because of lower base speed
+								vec31 = vec31.normalize().scale(1.6).add(vec3.scale(0.2));
+							}
+						}
 
-			if (strafingTime > -1) {
-				if (targetDistance > strafeRadiusSqr * 0.75) strafingBackwards = false;
-				else if (targetDistance < strafeRadiusSqr * 0.25) strafingBackwards = true;
-
-				Entity vehicle = mob.getControlledVehicle();
-				if (vehicle instanceof Mob) {
-					Mob mob = (Mob) vehicle;
-					mob.lookAt(target, 30, 30);
-				}
-
-				mob.lookAt(target, 30, 30);
+						buny.setDeltaMovement(vec31.x, Math.max(0, target.getY() - buny.getY()) * 0.2 + 0.4, vec31.z);
+						//TODO dedicated jump sound
+						mob.playSound(SoundEvents.RABBIT_JUMP, 1, 1);
+					}
+					else {
+						buny.getNavigation().stop();
+						//buny.getLookControl().setLookAt(target.getX(), target.getEyeY(), target.getZ());
+					}
+					break;
+				case 2:
+					//in the air
+					if (time <= 0 || buny.onGround()) {
+						//landed, switch to recovery
+						phase = 3;
+						time = 10;
+						buny.setAnimation(ANIM_LAND);
+						buny.getNavigation().stop();
+					}
+					else {
+						super.checkAndPerformAttack(target, distanceSqr);
+					}
+					break;
+				case 3:
+					//recovery
+					if (time <= 0) {
+						phase = 0;
+						//check if we'll chain another jump or shooting
+						if (target.hasEffect(BPASEffects.marked.get()) || extraShots > 0) {
+							if (buny.getSensing().hasLineOfSight(target)) {
+								phase = 5;
+								time = 15;
+								buny.setAnimation(ANIM_AIM);
+								buny.getLookControl().setLookAt(target, 50, 0);
+								if (target.hasEffect(BPASEffects.marked.get())) {
+									if (extraShots <= 0) extraShots = buny.random.nextInt(2) + buny.random.nextInt(2);
+								}
+								else extraShots--;
+							}
+							else {
+								buny.setAnimation(ANIM_NEUTRAL);
+								extraShots = 0;
+							}
+						}
+						else if (distanceSqr < 20) {
+							phase = 1;
+							time = 10;
+							buny.setAnimation(ANIM_WINDUP);
+							buny.getLookControl().setLookAt(target, 50, 0);
+							feints = buny.random.nextInt(2) + buny.random.nextInt(2); //0-2 feints, weighted towards 1
+							extraShots = 0;
+						}
+						else {
+							buny.setAnimation(ANIM_NEUTRAL);
+							extraShots = 0;
+						}
+					}
+					else buny.getNavigation().stop();
+					break;
+				case 4:
+					//in the air but feinting
+					if (time <= 0 || buny.onGround()) {
+						//landed, switch to recovery
+						phase = 1;
+						time = 5;
+						buny.getNavigation().stop();
+						buny.getLookControl().setLookAt(target, 30, 30);
+					}
+					break;
+				case 5:
+					//aiming for a shot
+					if (time <= 0) {
+						//shoot! but only if los
+						if (!buny.getSensing().hasLineOfSight(target)) {
+							phase = 0;
+							buny.setAnimation(ANIM_NEUTRAL);
+						}
+						else {
+							ItemStack gun = buny.getOffhandItem();
+							if (!gun.isEmpty() && gun.getItem() instanceof GunItem gunItem) {
+								ItemStack bullet = buny.getBulletStack();
+								gunItem.shootAt(buny, target, gun, bullet, buny.getBullet(), buny.getAddedSpread(), true);
+								buny.playSound(gunItem.getFireSound(), 1, 1.0F / (buny.getRandom().nextFloat() * 0.4F + 0.8F));
+							}
+							//switch to recovery
+							phase = 3;
+							time = 5;
+							buny.setAnimation(ANIM_SHOOTED);
+							buny.getNavigation().stop();
+							//cheating a bit on the gun fire delay
+							//TODO don't do that?
+						}
+					}
+					else {
+						buny.getNavigation().stop();
+						buny.getLookControl().setLookAt(target, 50, 0);
+					}
+					break;
 			}
-			else {
-				mob.getLookControl().setLookAt(target, 30, 30);
-			}
+		}
+
+		@Override
+		public boolean canContinueToUse() {
+			if (phase > 0 && time > 0) return true;
+			return super.canContinueToUse();
 		}
 	}
 }
--- src/main/java/lykrast/bypowderandsteel/renderer/ZombunnyInquisitorModel.java
+++ src/main/java/lykrast/bypowderandsteel/renderer/ZombunnyInquisitorModel.java
@@ -1,6 +1,7 @@
 package lykrast.bypowderandsteel.renderer;
 
 import lykrast.bypowderandsteel.entity.ZombunnyInquisitorEntity;
+import lykrast.bypowderandsteel.misc.BPASUtils;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
@@ -12,7 +13,9 @@ import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.HumanoidArm;
 
-public class ZombunnyInquisitorModel extends HumanoidModel<ZombunnyInquisitorEntity> {		
+public class ZombunnyInquisitorModel extends HumanoidModel<ZombunnyInquisitorEntity> {
+	private float animProgress;
+	
 	public ZombunnyInquisitorModel(ModelPart modelpart) {
 		super(modelpart);
 	}
@@ -27,19 +30,95 @@ public class ZombunnyInquisitorModel extends HumanoidModel<ZombunnyInquisitorEnt
 		
 		return LayerDefinition.create(meshdefinition, 64, 64);
 	}
+	
+	//TODO shooting animations, for now it's just slasher animations
+	
+	@Override
+	public void prepareMobModel(ZombunnyInquisitorEntity entity, float limbSwing, float limbSwingAmount, float partialTick) {
+		//the super is empty
+		animProgress = entity.getAnimProgress(partialTick);
+		animProgress = BPASUtils.easeOutQuad(animProgress);
+	}
+	
+	private float getTargetXRot(int anim, float neutral, boolean leftArm, boolean righthanded, float headPitch) {
+		//take the neutral because idle is what the humanoid tries to set
+		switch (anim) {
+			default:
+			case ZombunnyInquisitorEntity.ANIM_NEUTRAL:
+			case ZombunnyInquisitorEntity.ANIM_LAND:
+				return neutral;
+			case ZombunnyInquisitorEntity.ANIM_WINDUP:
+				return 70*Mth.DEG_TO_RAD;
+			case ZombunnyInquisitorEntity.ANIM_JUMP:
+				return -90*Mth.DEG_TO_RAD;
+			case ZombunnyInquisitorEntity.ANIM_AIM:
+				if (leftArm == righthanded) {
+					//gun in offhand
+					return headPitch * Mth.DEG_TO_RAD - Mth.HALF_PI;
+				}
+				else return neutral;
+			case ZombunnyInquisitorEntity.ANIM_SHOOTED:
+				if (leftArm == righthanded) {
+					//gun in offhand
+					return headPitch * Mth.DEG_TO_RAD - (Mth.HALF_PI*1.25f);
+				}
+				else return neutral;
+		}
+	}
+	
+	private float getTargetYRot(int anim, float neutral, boolean leftArm, boolean righthanded, float netHeadYaw) {
+		//take the neutral because idle is what the humanoid tries to set
+		switch (anim) {
+			default:
+			case ZombunnyInquisitorEntity.ANIM_NEUTRAL:
+				return neutral;
+			case ZombunnyInquisitorEntity.ANIM_AIM:
+			case ZombunnyInquisitorEntity.ANIM_SHOOTED:
+				if (leftArm == righthanded) {
+					//gun in offhand
+					return netHeadYaw * Mth.DEG_TO_RAD;
+				}
+				else return neutral;
+		}
+	}
+	
+	private float getTargetZRot(int anim, float neutral, boolean leftArm, boolean righthanded) {
+		//take the neutral because idle is what the humanoid tries to set
+		switch (anim) {
+			default:
+			case ZombunnyInquisitorEntity.ANIM_NEUTRAL:
+			case ZombunnyInquisitorEntity.ANIM_LAND:
+				return neutral;
+			case ZombunnyInquisitorEntity.ANIM_WINDUP:
+				return (leftArm ? -40 : 40)*Mth.DEG_TO_RAD;
+			case ZombunnyInquisitorEntity.ANIM_JUMP:
+				return (leftArm ? -75 : 75)*Mth.DEG_TO_RAD;
+		}
+	}
 
-	//TODO real animations, for now just copying the gunner
 	@Override
 	public void setupAnim(ZombunnyInquisitorEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-		crouching = entity.isCrouching();
-		rightArmPose = HumanoidModel.ArmPose.EMPTY;
-		leftArmPose = HumanoidModel.ArmPose.EMPTY;
-		if (!entity.getMainHandItem().isEmpty() && entity.isAggressive()) {
-			//swapped cause gun is in off hand
-			if (entity.getMainArm() == HumanoidArm.LEFT) rightArmPose = HumanoidModel.ArmPose.BOW_AND_ARROW;
-			else leftArmPose = HumanoidModel.ArmPose.BOW_AND_ARROW;
-		}
+		int anim = entity.clientAnim;
+		crouching = (anim >= ZombunnyInquisitorEntity.ANIM_WINDUP && anim <= ZombunnyInquisitorEntity.ANIM_LAND);
+		boolean righthanded = entity.getMainArm() == HumanoidArm.RIGHT;
 		super.setupAnim(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+		if (animProgress > 0.999) {
+			leftArm.xRot = getTargetXRot(anim, leftArm.xRot, true, righthanded, headPitch);
+			leftArm.yRot = getTargetYRot(anim, leftArm.yRot, true, righthanded, netHeadYaw);
+			leftArm.zRot = getTargetZRot(anim, leftArm.zRot, true, righthanded);
+			rightArm.xRot = getTargetXRot(anim, rightArm.xRot, false, righthanded, headPitch);
+			rightArm.yRot = getTargetYRot(anim, rightArm.yRot, false, righthanded, netHeadYaw);
+			rightArm.zRot = getTargetZRot(anim, rightArm.zRot, false, righthanded);
+		}
+		else {
+			int prev = entity.prevAnim;
+			leftArm.xRot = BPASUtils.rotlerpRad(animProgress, getTargetXRot(prev, leftArm.xRot, true, righthanded, headPitch), getTargetXRot(anim, leftArm.xRot, true, righthanded, headPitch));
+			leftArm.yRot = BPASUtils.rotlerpRad(animProgress, getTargetYRot(prev, leftArm.yRot, true, righthanded, netHeadYaw), getTargetYRot(anim, leftArm.yRot, true, righthanded, netHeadYaw));
+			leftArm.zRot = BPASUtils.rotlerpRad(animProgress, getTargetZRot(prev, leftArm.zRot, true, righthanded), getTargetZRot(anim, leftArm.zRot, true, righthanded));
+			rightArm.xRot = BPASUtils.rotlerpRad(animProgress, getTargetXRot(prev, rightArm.xRot, false, righthanded, headPitch), getTargetXRot(anim, rightArm.xRot, false, righthanded, headPitch));
+			rightArm.yRot = BPASUtils.rotlerpRad(animProgress, getTargetYRot(prev, rightArm.yRot, false, righthanded, netHeadYaw), getTargetYRot(anim, rightArm.yRot, false, righthanded, netHeadYaw));
+			rightArm.zRot = BPASUtils.rotlerpRad(animProgress, getTargetZRot(prev, rightArm.zRot, false, righthanded), getTargetZRot(anim, rightArm.zRot, false, righthanded));
+		}
 	}
 
 }
